45b855199a0dbc66bb24889ad31a964ff4349c33
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-03-01 01:00:41 +0200

    Implemented initial "Alert"-styled game end (temp)

--- script.js
+++ script.js
@@ -26,7 +26,10 @@ let checkDead = setInterval(function () {
     let blockLeft = parseInt(window.getComputedStyle(block)
         .getPropertyValue("left"));
     if (blockLeft < 20 && blockLeft > -20 && characterTop >= 130) {
-        
+        block.style.animation = "none";
+        alert("Game Over! SCORE: " + Math.floor(counter / 100));
+        counter = 0;
+        block.style.animation = "block 2s infinite linear";
         
        
        
