4ca959ed899646b47c043de19474ef8338e78164
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-03-01 00:57:14 +0200

    Added event listener for the jump click

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ let character = document.getElementById("character");
 let block = document.getElementById("block");
 let counter = 0;
 let listener = document.querySelector('html');
+let gameENDED = false;
+let newGameBtnElement = null;
+listener.addEventListener('click', jump);
 
-
+function jump() {
+   
+   
+}
 
